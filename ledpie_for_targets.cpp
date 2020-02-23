7240374223f21526d198a6893ce5676695c1fe53
sjdevlin <sjdevlin@gmail.com>
2020-02-23 15:33:52 +0000

    total rewrite.  Working well

--- ledpie_for_targets.cpp
+++ ledpie_for_targets.cpp
@@ -12,73 +12,95 @@
 
 namespace hal = matrix_hal;
 
-//pots = potential sources
-
-// LOCATIONS_COUNT : Number of locations where sound can come from.
-#define LOCATIONS_COUNT 54
+#define ANGLE_SPREAD 20  // Gives 40 degree window for each speaker
 // MAX_VALUE : max value of energy
-#define MAX_VALUE 500
-// INCREMENT : multipler to amplify change in odas E value
-#define INCREMENT 40
-// DECREMENT : controls delay in the dimming
-#define DECREMENT 1
-// MIN_THRESHOLD: Filters out low energy targets from odas
 #define MIN_THRESHOLD 50
-// MAX_BRIGHTNESS: 0 - 255
+// MAX_BRIGHTNESS: 0 - 20
 #define MAX_BRIGHTNESS 12
 // MAX_PARTICIPANTS: Max number of people in meeting
-#define MAX_PARTICIPANTS 9
-
+#define MAX_PARTICIPANTS 6
 
 //these variables hold the value from odas of x,y and energy_array
 double x, y, z, E;
 int tracked_source_id;
 
-int position_person_number[LOCATIONS_COUNT];
-int total_talk_time[MAX_PARTICIPANTS];
+struct meeting_member {
+  int led_num;
+  int angle;
+  int angle_low; int angle_high; 
+  bool talking; 
+  bool was_talking; 
+  long total_talk_time;
+  char sex;
+  int num_turns;};
+  
+  struct meeting_member participant [MAX_PARTICIPANTS];
+
+long total_meeting_duration=0;
 int num_participants = 0;
-int energy_array[LOCATIONS_COUNT];
 
 struct rgb_value {int red; int green; int blue;} ;
-struct rgb_value participant_colour[MAX_PARTICIPANTS] = {
+struct rgb_value colour[MAX_PARTICIPANTS] = {
   {12,0,0},
   {0,12,0},
-  {0,0,12},
+  {0,0,9},
   {6,6,0},
-  {6,0,6},
-  {0,6,6},
-  {4,4,2},
-  {4,2,4},
-  {2,4,4} };
+  {6,0,5},
+  {0,6,5}};
 
-
+/*
 const double led_angles_in_matrixvoice[18] = {170, 150, 130, 110, 90,  70,
                                       50,  30,  10,  350, 330, 310,
                                       290, 270, 250, 230, 210, 190};
+*/
+
+
 
 void capture_energy_level_at_location() {
+
+  int num_matches=0;
+  int person_speaking=-1;
   // Convert x,y to angle. TODO: See why x axis from ODAS is inverted
-  double angle_xy = fmodf((atan2(y, x) * (180.0 / M_PI)) + 360, 360);
-  // Convert angle to index
-  int i_angle = angle_xy / 360 * LOCATIONS_COUNT;  // convert degrees to index
-  // Set energy for this angle
-  energy_array[i_angle] += INCREMENT ;
-  // Set limit at MAX_VALUE
-  energy_array[i_angle] =
-      energy_array[i_angle] > MAX_VALUE ? MAX_VALUE : energy_array[i_angle];
+  int angle_xy = (atan2(y, x) * (180.0 / M_PI)) ;
+  //check is angle already recorded
+  for (int i=0;i<num_participants;i++) {
+  // check how far away it is from a known source
+    int angle_diff = participant[i].angle - angle_xy;
+    angle_diff += (angle_diff>180) ? -360 : (angle_diff<-180) ? 360 : 0;
+    if (abs(angle_diff) < ANGLE_SPREAD) {
+      ++num_matches;
+      person_speaking = i;}
+      // we can here start to average out the location for a speaker
+}
+// end of checking through existing
+
+   if (num_matches == 0) {
+// new participant
+     participant[num_participants].angle = angle_xy;
+     participant[num_participants].led_num = (180-angle_xy)/20;
+     participant[num_participants].talking = true;
+     participant[num_participants].num_turns = 1;
+     participant[num_participants].total_talk_time = 0;
+     printf ("new member %d at %d degrees.  LED number: %d \n", num_participants,angle_xy, participant[num_participants].led_num);
+     ++num_participants;
+    };    
+
+   if (num_matches == 1) {
+// add to existing participant
+     participant[person_speaking].talking = true;
+     participant[person_speaking].num_turns += (participant[person_speaking].was_talking) ? 0 : 1;
+     ++participant[person_speaking].total_talk_time;
+    };    
+   if (num_matches > 1) printf ("error overlap of %d people\n", num_matches);
 }
 
-void fade_energy_levels() {
-  for (int i = 0; i < LOCATIONS_COUNT; i++) {
-    energy_array[i] -= (energy_array[i] > 0) ? DECREMENT : 0;
-  }
-
-}
 
 void json_parse_array(json_object *jobj, char *key) {
   // Forward Declaration
   void json_parse(json_object * jobj);
   enum json_type type;
+//  printf("have gone into parse array!!\n");
+
   json_object *jarray = jobj;
   if (key) {
     if (json_object_object_get_ex(jobj, key, &jarray) == false) {
@@ -107,7 +129,6 @@ void json_parse_array(json_object *jobj, char *key) {
 void json_parse(json_object *jobj) {
   enum json_type type;
   unsigned int count = 0;
-  fade_energy_levels();  // drop LED value by a little each time you read a json for fade effect
   json_object_object_foreach(jobj, key, val) {
     type = json_object_get_type(val);
     switch (type) {
@@ -152,18 +173,6 @@ void json_parse(json_object *jobj) {
 
 int main(int argc, char *argv[]) {
 
-
-// initialise arrays
-
-  for (int i =0; i<LOCATIONS_COUNT;i++) {
-       position_person_number[i]=-1;
-      }
-
-  for (int i =0; i<MAX_PARTICIPANTS;i++) {
-       total_talk_time[i]=0;
-    //  participant_is_talking[i]=false
-            }
-
   // Everloop Initialization
   hal::MatrixIOBus bus;
   if (!bus.Init()) return false;
@@ -180,104 +189,60 @@ int main(int argc, char *argv[]) {
   }
   everloop.Write(&image1d);
 
-  char verbose = 0x00;
-
   int server_id;
   struct sockaddr_in server_address;
   int connection_id;
   char *message;
   int messageSize;
-
-  int c;
   unsigned int portNumber = 9000;
   const unsigned int nBytes = 10240;
 
   server_id = socket(AF_INET, SOCK_STREAM, 0);
-
   server_address.sin_family = AF_INET;
   server_address.sin_addr.s_addr = htonl(INADDR_ANY);
   server_address.sin_port = htons(portNumber);
-
-  printf("Binding socket........... ");
   fflush(stdout);
   bind(server_id, (struct sockaddr *)&server_address, sizeof(server_address));
-  printf("[OK]\n");
-
-  printf("Listening socket......... ");
   fflush(stdout);
   listen(server_id, 1);
-  printf("[OK]\n");
-
-  printf("Waiting for connection in port %d ... ", portNumber);
   fflush(stdout);
   connection_id = accept(server_id, (struct sockaddr *)NULL, NULL);
-  printf("[OK]\n");
-
   message = (char *)malloc(sizeof(char) * nBytes);
 
   printf("Receiving data........... \n\n");
-
-
-  // this is where we put code to reset the pie for new meeting
-  // either by pbutton press or by long silence
 
   while ((messageSize = recv(connection_id, message, nBytes, 0)) > 0) {
     message[messageSize] = 0x00;
 
     //printf("message: %s\n\n", message);
+
+// reset all particpants to be silent - but record if they were talking in last message set
+  for (int i=0;i<num_participants;i++) {
+     participant[i].was_talking = participant[i].talking ;  // store value  
+     participant[i].talking = false; // then reset
+   }
+
     json_object *jobj = json_tokener_parse(message);
     json_parse(jobj);
 
     // each time there is a json message recalc people and positions
     // using the energy array - the energy array is set by cyclng through
     // all the potential sources in json message
+      
 
-    for (int i = 0; i< LOCATIONS_COUNT; i++) {
-
-    if  (energy_array[i] > MIN_THRESHOLD) {
-        // someone is talking
-        // if from a posiiton that is already assigned threads_single_open
-        // increment the speak time for that person
-        if (position_person_number[i]>-1) {
-        total_talk_time[position_person_number[i]]++;
-  //      participant_is_talking[position_person_number[i]] = true
-        }
-        else
-        // else assign that area (+-10 degrees) to a new participant
-        // and increment num_participants to reflect new person
-        {
-
-          for (int j=-2;j<3;j++) {
-              int k = i+j;
-              if (k<0) k+=LOCATIONS_COUNT;
-              if (k>=LOCATIONS_COUNT) k-=LOCATIONS_COUNT;
-              if (position_person_number[k] == -1) {
-                position_person_number[k] = num_participants;
-                              printf ("surrounding k %d \n", k);}
-         }
-              ++num_participants;
-     
-}
-      }
-    }
-
-
-// re code this below
-    for (int i = 0; i < bus.MatrixLeds(); i++) {
+    for (int i = 0; i < num_participants; i++) {
       // Convert from angle to pots index
-      int index_pots = led_angles_in_matrixvoice[i] * LOCATIONS_COUNT / 360;
-      // Mapping from pots values to color
-      int brightness = energy_array[index_pots] * MAX_BRIGHTNESS / MAX_VALUE;
-      // Removing colors below the threshold
-      if (position_person_number[index_pots] > -1){
-      image1d.leds[i].red = brightness * participant_colour[position_person_number[index_pots]].red;
-      image1d.leds[i].green = brightness * participant_colour[position_person_number[index_pots]].green;
-      image1d.leds[i].blue = brightness * participant_colour[position_person_number[index_pots]].blue;
-      image1d.leds[i].white = 0;
+      int brightness = (participant[i].talking) ? MAX_BRIGHTNESS : 0;
+      image1d.leds[participant[i].led_num].red = brightness * colour[i].red;
+      image1d.leds[participant[i].led_num].green = brightness * colour[i].green;
+      image1d.leds[participant[i].led_num].blue = brightness * colour[i].blue;
+      image1d.leds[participant[i].led_num].white = 0;
+      
 
-      printf ("led %d index %d person %d brightness %d \n", i,index_pots, position_person_number[index_pots],brightness);
+      //printf ("led %d index %d person %d brightness %d \n", i,index_pots, position_person_number[index_pots],brightness);
     }
-    }
+ 
     everloop.Write(&image1d);
-  }
+ 
+}
 }
